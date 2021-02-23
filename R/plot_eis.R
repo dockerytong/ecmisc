63f1ac5478f983c1f258ed9d4bd38b7f0bdd287f
dockerytong <liutong369@gmail.com>
2021-02-23 18:39:53 +0800

    fix bugs

--- R/plot_eis.R
+++ R/plot_eis.R
@@ -3,38 +3,46 @@
 #' @param data EIS data for plot
 #' @param type plot type desired, N for Nyquist plot and B for Bode plot, default as Nyquist
 #' @param size point size, default as 1
-#' @param asp plot aspect ratio default as 1
+#' @param asp aspect ratio of plot
 #' @param color point color, default as blue
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plot_eis <- function(data, type = 'N', size = 1, color = 'blue', asp = 1) {
+plot_eis <- function(data, type = 'N', size = 1, asp = 1, color = 'Sample') {
+  p <- ggplot(data = data) + pub_theme(asp)
   if (type == 'N') {
-    ggplot(data = data) +
-      geom_point(aes(Re, -Im), shape = 21, size = size, color = color) +
-      labs(x = 'Re (ohm)',
-           y = '-Im (ohm)') +
-      pub_theme(asp)
+    p + geom_point(
+      aes(x = Re,y = -Im, color = .data[[color]]),
+      shape = 21,
+      size = size) +
+      labs(
+        x = 'Re (ohm)',
+        y = '-Im (ohm)'
+      )
   } else if (type == 'B') {
-    p1 <- ggplot(data = data) +
-      geom_point(aes(Frequency,Magnitude),shape = 21,size = size, color = color) +
+    p1 <- p + geom_point(aes(x = Frequency, y = Magnitude, color = .data[[color]]),
+      shape = 21,
+      size = size) +
       scale_x_log10(
-        breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))
+        breaks = scales::trans_breaks("log10", function(x)
+          10 ^ x),
+        labels = scales::trans_format("log10", scales::math_format(10 ^ .x))
       ) +
       scale_y_log10(
-        breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))
+        breaks = scales::trans_breaks("log10", function(x)
+          10 ^ x),
+        labels = scales::trans_format("log10", scales::math_format(10 ^ .x))
       ) +
-      labs(
-        x = 'Frequency (Hz)',
-        y = 'Magnitude (ohm)'
-      ) +
-      pub_theme(asp)
-    p2 <- ggplot(data = data) +
-      geom_point(aes(Frequency,-Phase),shape = 21,size = size, color = color) +
+      labs(x = 'Frequency (Hz)',
+           y = 'Magnitude (ohm)')
+
+    p2 <- p +
+      geom_point(
+        aes(x = Frequency, y = -Phase, color = .data[[color]]),
+        shape = 21,
+        size = size) +
       scale_x_log10(
         breaks = scales::trans_breaks("log10", function(x) 10^x),
         labels = scales::trans_format("log10", scales::math_format(10^.x))
@@ -42,10 +50,9 @@ plot_eis <- function(data, type = 'N', size = 1, color = 'blue', asp = 1) {
       labs(
         x = 'Frequency (Hz)',
         y = '-Phase (degree)'
-      ) +
-      pub_theme(asp)
-    p1+p2
-  } else {
-    echo('Wrong plot type input!')
+      )
+
+    p <- p1 + p2 + plot_layout(guides = 'collect')
+    p
   }
 }
